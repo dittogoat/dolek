6fd60d1826ade17e08fa2f79611a7a9b66560116
Dominik Knyt <11154720u@zs1piaseczno.local>
2025-10-28 14:00:53 +0100

    moja supcio apka v1

--- w_node/index.js
+++ w_node/index.js
@@ -1,2 +1,26 @@
 import express from 'express';
 const app = express();
+
+const port = 4000;
+
+app
+.get('/',(req,res) => {
+    res.send('test');
+})
+.get('/test',(req,res)=>{
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.write('siemano')
+    res.end()
+})
+.listen(port,()=>{
+    console.log('uruchomiono')
+})
+
