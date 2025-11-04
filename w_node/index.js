c5dfcc84371c6738ebabe5779b373712dccfc5f8
Dominik Knyt <11154720u@zs1piaseczno.local>
2025-11-04 14:08:39 +0100

    id

--- w_node/index.js
+++ w_node/index.js
@@ -4,24 +4,23 @@ const app = express();
 const port = 4000;
 
 app
-.get('/',(req,res) => {
-    res.send('test');
-})
-.get('/test',(req,res)=>{
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.write('siemano')
-    res.end()
-})
-.listen(port,()=>{
-    console.log('uruchomiono')
+.get('/', (req, res) => {
+res.send('test');
 })
 
-// test
+.get('/test', (req, res) => {
+for (let i = 0; i < 10; i++) {
+    res.write('siemano<br>');
+}
+res.end();
+})
+
+.get('/user/:id', (req, res) => {
+    console.log('GET /user/:id');
+    res.write(req.params.id)
+    res.end()
+})
+
+.listen(port, () => {
+console.log(`Serwer uruchomiono na porcie ${port}`);
+});
